76e8511a3f39fd1b819deb9ff7ebb9f56abd53f8
Roger Leite Lucena <rogerleitelucena@gmail.com>
2019-11-14 16:06:27 +0100

    Finish section 2.4

--- TDs/4-5/src/circlesV3.py
+++ TDs/4-5/src/circlesV3.py
@@ -7,28 +7,30 @@ def init_model(nx, nh, ny):
     model = torch.nn.Sequential(
         torch.nn.Linear(nx, nh),
         torch.nn.Tanh(),
-        torch.nn.Linear(nh, ny),
-        torch.nn.Softmax(dim=1)
+        torch.nn.Linear(nh, ny)
+        # torch.nn.Softmax()
     )
 
     loss = torch.nn.CrossEntropyLoss()
     return model, loss
 
 
-def loss_accuracy(Yhat, Y, loss):
+def loss_accuracy(Ytilde, Y, loss):
     # TODO
-    # L = ((-1)*torch.log(Yhat)*Y).sum()
-    L = loss(Yhat, Y)
 
+    Yhat = torch.nn.Softmax()(Ytilde)
     Yprevs = torch.argmax(Yhat, dim=1) # nbatch x 1
     # print('Yprevs.shape: ', Yprevs.shape)
     # print("Y.sum():\n", Y.sum(dim=0))
     # print("Yprevs:\n", Yprevs)
     # print('Yprevs.sum(): ', Yprevs.sum())
-    Yargmax = torch.argmax(Y, dim=1)
+    Yargmax = torch.argmax(Y, dim=1) # nbatch x 1
     acc = (Yprevs == Yargmax).sum().item() / Y.shape[0]
     # print("L: ", L)
+
+    # L = ((-1)*torch.log(Yhat)*Y).sum()
     # print("acc: ", acc)
+    L = loss(Ytilde, Yargmax)
     return L, acc
 
 
@@ -49,12 +51,12 @@ if __name__ == '__main__':
     data = CirclesData()
     # data.plot_data()
     N = data.Xtrain.shape[0]
-    Nepoch = 1500  # 3000
+    Nepoch = 800 # 1500  # 3000
     Nbatch = 10
     nx = data.Xtrain.shape[1]
     nh = 10
     ny = data.Ytrain.shape[1]
-    eta = 0.2
+    eta = 0.02
     print('Data set size: ', N)  # 200 here
     # params = init_params(nx, nh, ny)
     model, loss = init_model(nx, nh, ny)
@@ -67,11 +69,11 @@ if __name__ == '__main__':
         for j in range(int(N/Nbatch)):
             X = torch.from_numpy(data._Xtrain[j * Nbatch : (j+1) * Nbatch])
             Y = torch.from_numpy(data._Ytrain[j * Nbatch : (j+1) * Nbatch])
-            
-            # Yhat, outputs = forward(params, X)
-            Yhat = model(X)
 
-            L, accuracy = loss_accuracy(Yhat, Y, loss)
+            # Yhat, outputs = forward(params, X)
+            Ytilde = model(X)
+
+            L, accuracy = loss_accuracy(Ytilde, Y, loss)
 
             # grads = backward(params, outputs, Y)
             L.backward()
@@ -82,21 +84,22 @@ if __name__ == '__main__':
             print('- Epoch: ', i)
             # Training set
             # Yhat, _ = forward(params, torch.from_numpy(data._Xtrain))
-            Yhat = model(torch.from_numpy(data._Xtrain))
-            L, accuracy = loss_accuracy(Yhat, torch.from_numpy(data._Ytrain), loss)
+            Ytilde = model(torch.from_numpy(data._Xtrain))
+            L, accuracy = loss_accuracy(Ytilde, torch.from_numpy(data._Ytrain), loss)
             trainLosses.append(L.item())
             print(' Training set:\n', '   loss: ', L.item(), '; accuracy: ', accuracy, sep='')
 
             # Testing set
             # Yhat, _ = forward(params, torch.from_numpy(data._Xtest))
-            Yhat = model(torch.from_numpy(data._Xtest))
-            L, accuracy = loss_accuracy(Yhat, torch.from_numpy(data._Ytest), loss)
+            Ytilde = model(torch.from_numpy(data._Xtest))
+            L, accuracy = loss_accuracy(Ytilde, torch.from_numpy(data._Ytest), loss)
             print(' Test set:\n', '   loss: ', L.item(), '; accuracy: ', accuracy, sep='')
             testLosses.append(L.item())
 
             # Ygrid, _ = forward(params, data.Xgrid)
-            Ygrid = model(data.Xgrid)
-            data.plot_data_with_grid(Ygrid.detach())
+            Ygridtilde = model(data.Xgrid)
+            Ygridhat = torch.nn.Softmax()(Ygridtilde) # Softmax is not inside the "model" because "torch.nn.CrossEntropyLoss()" already has it, so you have to do this step to transform "Ygridtilde" in "Ygridhat" 
+            data.plot_data_with_grid(Ygridhat.detach())
 
     x = np.arange(0, Nepoch, printInterval)
     trainLosses = np.array(trainLosses)
