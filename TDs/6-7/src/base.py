e3e098497c93be8d4091e45a361277efc41f74e6
Roger Leite Lucena <rogerleitelucena@gmail.com>
2019-11-16 01:35:06 +0100

    Working on TME6-7

--- TDs/6-7/src/base.py
+++ TDs/6-7/src/base.py
@@ -27,6 +27,46 @@ class ConvNet(nn.Module):
         # On défini d'abord les couches de convolution et de pooling comme un
         # groupe de couches `self.features`
         self.features = nn.Sequential(
+            nn.Conv2d(1, 32, (5, 5), stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), stride=2, padding=0),
+            nn.Conv2d(32, 64, (5, 5), stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), stride=2, padding=0),
+            nn.Conv2d(64, 64, (5, 5), stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), stride=2, padding=0),
+        )
+        # On défini les couches fully connected comme un groupe de couches
+        # `self.classifier`
+        self.classifier = nn.Sequential(
+            nn.Linear(576, 1000),
+            nn.ReLU(),
+            nn.Linear(1000, 10),
+            # nn.Softmax(),
+            # Rappel : Le softmax est inclus dans la loss, ne pas le mettre ici
+        )
+
+    # méthode appelée quand on applique le réseau à un batch d'input
+    def forward(self, input):
+        bsize = input.size(0) # taille du batch
+        output = self.features(input) # on calcule la sortie des conv
+        output = output.view(bsize, -1) # on applati les feature map 2D en un
+                                        # vecteur 1D pour chaque input
+        output = self.classifier(output) # on calcule la sortie des fc
+        return output
+
+# Original below:
+class ConvNetLeNet5(nn.Module):
+    """
+    Cette classe contient la structure du réseau de neurones
+    """
+
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        # On défini d'abord les couches de convolution et de pooling comme un
+        # groupe de couches `self.features`
+        self.features = nn.Sequential(
             nn.Conv2d(1, 6, (5, 5), stride=1, padding=2),
             nn.Tanh(),
             nn.MaxPool2d((2, 2), stride=2, padding=0),
